1cd5f2e56bba204c30bc5a6fa5bf9dfa0884c997
aryan-jadon <aryan.jadon@sjsu.edu>
2021-09-21 15:12:10 -0700

    added 2 test cases

--- twitter_services/tests.py
+++ twitter_services/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000/')
+        # response status code 200 - Running OK
+        self.assertEqual(response.status_code, 200)
+
+    def test_tweet_page_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000/create/')
+        # response status code 200 - Running OK
+        self.assertEqual(response.status_code, 200)
+
+
+# which html pages are we using - index.html , create_tweet.html
