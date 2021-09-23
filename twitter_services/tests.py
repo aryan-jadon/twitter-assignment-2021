004c2e232bb243a9c8c072b7a3e511111f52b24d
Shreya-07 <60109870+Shreya-07@users.noreply.github.com>
2021-09-23 16:43:04 -0700

    Update tests.py
    Template Test Cases

--- twitter_services/tests.py
+++ twitter_services/tests.py
@@ -14,5 +14,17 @@ class ViewsTestCase(TestCase):
         # response status code 200 - Running OK
         self.assertEqual(response.status_code, 200)
 
+# Template Test Cases
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('index_page'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
 
-# which html pages are we using - index.html , create_tweet.html
+    def test_about_page_contains_correct_html(self):
+        response = self.client.get('http://127.0.0.1:8000/')
+        self.assertContains(response, '<h1>Welcome To Twitter</h1>')
+
+    def test_about_page_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(
+            response, 'Hi there! I should not be on the page.')
